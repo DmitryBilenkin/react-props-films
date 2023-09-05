0bb68b5ba235f613919006293e9ed1192d428f7f
Dmitry Bilenkin <dbilenkin@yandex.ru>
2023-09-05 22:15:23 +0300

    2red

--- src/components/Stars.tsx
+++ src/components/Stars.tsx
@@ -13,7 +13,7 @@ function Stars(props:StarsProps) {
     const[count, setCount] = useState<number>(props.count);
     const[starsArray, setStarsArray] = useState<number[]>(initialStarsArray);
 
-    const randomStarsHandler = () =>{
+    const randomStarsHandler = ():void =>{
         setCount(Math.floor(Math.random() * (5 - 0 + 1)));
         setStarsArray(new Array(count).fill(Math.random().toFixed(1)));
 
